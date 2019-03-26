8c448d35e6d68abba3cdaaf4d6378ed7521d3491
tuda2008 <1756991323@qq.com>
2019-03-26 22:39:50 +0800

    update messages

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -8,28 +8,28 @@ class Api::V1::MessagesController < ApplicationController
     datas = []
   	if params[:device_id]
       if query_type == 1
-        @messages = Message.visible.today.where(user_id: @user.id, device_id: params[:device_id]).includes(:device).page(params[:page]).per(10)
+        @messages = Message.visible.today.where(user_id: @user.id, device_id: params[:device_id]).page(params[:page]).per(10)
       elsif query_type == 2
-        @messages = Message.visible.yesterday.where(user_id: @user.id, device_id: params[:device_id]).includes(:device).page(params[:page]).per(10)
+        @messages = Message.visible.yesterday.where(user_id: @user.id, device_id: params[:device_id]).page(params[:page]).per(10)
       else
-        @messages = Message.visible.last_week.where(user_id: @user.id, device_id: params[:device_id]).includes(:device).page(params[:page]).per(10)
+        @messages = Message.visible.last_week.where(user_id: @user.id, device_id: params[:device_id]).page(params[:page]).per(10)
       end
       @messages.each do |msg|
-        datas << { id: msg.id, oper_cmd: msg.oper_cmd,
-                   lock_num: msg.lock_num, lock_type: msg.lock_type,
-                   device_name: msg.device.name,
+        datas << { id: msg.id, oper_cmd: Message::CMD_NAMES[msg.oper_cmd],
+                   lock_num: msg.lock_num.blank? ? "" : msg.lock_num, lock_type: Message::TYPENAMES[msg.lock_type],
                    created_at: query_type==3 ? msg.created_at.strftime('%m-%d %H:%M:%S') : msg.created_at.strftime('%H:%M:%S')}
       end
   	else
       if query_type == 1
-        @messages = Message.visible.today.where(user_id: @user.id).page(params[:page]).per(10)
+        @messages = Message.visible.today.where(user_id: @user.id).includes(:device).page(params[:page]).per(10)
       elsif query_type == 2
-        @messages = Message.visible.yesterday.where(user_id: @user.id).page(params[:page]).per(10)
+        @messages = Message.visible.yesterday.where(user_id: @user.id).includes(:device).page(params[:page]).per(10)
       else
-        @messages = Message.visible.last_week.where(user_id: @user.id).page(params[:page]).per(10)
+        @messages = Message.visible.last_week.where(user_id: @user.id).includes(:device).page(params[:page]).per(10)
       end
       @messages.each do |msg|
         datas << { id: msg.id, oper_cmd: Message::CMD_NAMES[msg.oper_cmd],
+                   device_name: msg.device.name,
                    lock_num: msg.lock_num.blank? ? "" : msg.lock_num, lock_type: Message::TYPENAMES[msg.lock_type],
                    created_at: query_type==3 ? msg.created_at.strftime('%m-%d %H:%M:%S') : msg.created_at.strftime('%H:%M:%S')}
       end
